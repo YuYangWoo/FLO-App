2d23c423a560a48d99226e37c78a853502f4a668
YuYangWoo <59405161+YuYangWoo@users.noreply.github.com>
2021-06-03 11:43:03 +0900

    ExoPlayer 음원 파일 연동 및 UI 수정

--- flo/app/src/main/java/com/example/floapplication/ui/main/view/fragment/MainFragment.kt
+++ flo/app/src/main/java/com/example/floapplication/ui/main/view/fragment/MainFragment.kt
@@ -1,13 +1,9 @@
 package com.example.floapplication.ui.main.view.fragment
 
-import android.annotation.SuppressLint
-import android.net.Uri
 import android.util.Log
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.Observer
-import androidx.media2.exoplayer.external.ExoPlayerFactory
 import androidx.media2.exoplayer.external.SimpleExoPlayer
-import androidx.media2.exoplayer.external.source.ProgressiveMediaSource
-import androidx.media2.exoplayer.external.upstream.DefaultHttpDataSourceFactory
 import com.bumptech.glide.Glide
 import com.example.floapplication.R
 import com.example.floapplication.data.model.Resource
@@ -16,7 +12,17 @@ import com.example.floapplication.databinding.FragmentMainBinding
 import com.example.floapplication.ui.base.BaseFragment
 import com.example.floapplication.ui.main.view.dialog.ProgressDialog
 import com.example.floapplication.ui.main.viewmodel.SongViewModel
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.ui.PlayerView
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+import com.google.android.exoplayer2.SimpleExoPlayer.Builder
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.MediaSourceFactory
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     private val model: SongViewModel by sharedViewModel()
@@ -26,11 +32,12 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
     private val dialog by lazy {
         ProgressDialog(requireContext())
     }
-    private var player: SimpleExoPlayer? = null
+    private lateinit var simpleExoPlayer: com.google.android.exoplayer2.SimpleExoPlayer
 
     override fun init() {
         super.init()
         initViewModel()
+
     }
 
     private fun initViewModel() {
@@ -60,7 +67,6 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
         })
     }
 
-    @SuppressLint("RestrictedApi")
     private fun binding() {
         with(binding) {
             txtTitle.text = songResponse.title
@@ -69,9 +75,50 @@ class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
             Glide.with(requireContext()).load(songResponse.image).into(imgAlbum)
             songUrl = songResponse.file
         }
+        initializedPlayer()
+        initListeners()
+    }
 
+    private fun initializedPlayer() {
+        val mediaDataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(requireContext(), Util.getUserAgent(requireContext(), "mediaPlayerSample"))
+
+        val mediaSource = ProgressiveMediaSource.Factory(mediaDataSourceFactory).createMediaSource(
+            MediaItem.fromUri(songUrl))
+
+        val mediaSourceFactory: MediaSourceFactory = DefaultMediaSourceFactory(mediaDataSourceFactory)
+
+        simpleExoPlayer = com.google.android.exoplayer2.SimpleExoPlayer.Builder(requireContext())
+            .setMediaSourceFactory(mediaSourceFactory)
+            .build()
+
+        simpleExoPlayer.addMediaSource(mediaSource)
+        simpleExoPlayer.prepare()
+    }
+
+    private fun initListeners() {
+        var bool = false
+        binding.btnPlay.setOnClickListener {
+            if(!bool) {
+                Log.d(TAG, "initListeners: 플레이버튼누름")
+                bool = true
+                binding.btnPlay.background = resources.getDrawable(R.drawable.ic_baseline_pause_24, null)
+                simpleExoPlayer.playWhenReady = true
+                Log.d(TAG, "initListeners: 플레이버튼누름")
+
+            }
+            else {
+                Log.d(TAG, "initListeners: 정지버튼누름")
+                simpleExoPlayer.playWhenReady = false
+                binding.btnPlay.background = ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_play_arrow_24, null)
+                bool = false
+            }
+        }
 
     }
 
+    override fun onDestroy() {
+        simpleExoPlayer.playWhenReady = false
+        super.onDestroy()
+    }
 
 }
